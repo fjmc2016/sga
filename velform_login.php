f88dbf77a73f82afc003729bd8eca4d1be2698e4
fjmc2016 <fjmc2016@gmail.com>
2018-11-12 21:58:23 -0200

    Initial commit

---
+++ velform_login.php
@@ -0,0 +1,34 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Pagina de Login</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    
+  </head>
+
+  <body>
+<fieldset class="mt-5 col-12 mx-auto col-sm-4 card p-3">
+    
+  <h1> <legend class="text-center"  >Login</legend> </h1>
+  <form action="" method="post">
+    <div class="form-group">
+      <label for="iEmail" class="sr-only">Email</label>
+      <input type="email" id="iEmail" name="txtEmail" placeholder="Informe o email" class="form-control" required>
+    </div>
+    <div class="form-group">
+      <label for="iPass" class="sr-only">Senha</label>
+      <input type="password" id="iEiPassmail" name="txtPass" placeholder="Informe a senha" class="form-control" required>
+    </div>
+    
+      <button class="btn btn-dark btn-block col-12 col-sm-5">Entrar</button>
+    
+  </form>
+</fieldset>
+
+    
+
+    
+  </body>
+</html>
